8c622b532c85cb892de402fbbef2428fb40fb6ed
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-03-07 23:21:41 +0500

    make switch statement for logic

--- basic.java
+++ basic.java
@@ -1,17 +1,17 @@
-// import java.util.Scanner;
+import java.util.Scanner;
 
 public class basic {
     public static void main(String[] args){
         //chexk the condition is number even or odd?
-        // Scanner sc = new Scanner(System.in);
-        // System.out.println("Enter a number:");
-        // int number = sc.nextInt();
-        // if (number % 2 == 0) {
-        //     System.out.println("The number is even.");
-        // } else {
-        //     System.out.println("The number is odd.");
-        // }
-        // sc.close();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number:");
+        int number = sc.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("The number is even.");
+        } else {
+            System.out.println("The number is odd.");
+        }
+        sc.close();
 
 
 
@@ -19,5 +19,14 @@ public class basic {
         int marks = 56;
         String Reportcards = marks>33? "pass":"fail";
         System.out.println(Reportcards);
+    
+        // use switch statement
+        int n = 2;
+        switch(n){
+            case 1: System.out.println("samosa"); break;
+            case 2: System.out.println("burger"); break;
+            case 3: System.out.println("pizza"); break;
+            default: System.out.println("Invalid choice");
+        }
     }
 }
