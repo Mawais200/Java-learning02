31e8dd55f4e918dcd46adc9e02353652200409fc
Mawais200 <fa22-bsse-201@lgu.edu.pk>
2025-03-07 22:59:16 +0500

    make a logic if student have more that 33 per marks then printy pass

--- basic.java
+++ basic.java
@@ -1,17 +1,23 @@
-package java02;
-
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class basic {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter a number:");
-        int number = sc.nextInt();
-        if (number % 2 == 0) {
-            System.out.println("The number is even.");
-        } else {
-            System.out.println("The number is odd.");
-        }
-        sc.close();
+        //chexk the condition is number even or odd?
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter a number:");
+        // int number = sc.nextInt();
+        // if (number % 2 == 0) {
+        //     System.out.println("The number is even.");
+        // } else {
+        //     System.out.println("The number is odd.");
+        // }
+        // sc.close();
+
+
+
+        //check condition fail or pass by ternary operator
+        int marks = 56;
+        String Reportcards = marks>33? "pass":"fail";
+        System.out.println(Reportcards);
     }
 }
